07636df25ad3888ca5f557d3e7f4b9566847fd02
enqack <enqack@gmail.com>
2021-01-09 00:05:04 -0500

    Added mutimonitor support.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,6 +4,7 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bottom &
+polybar primary_bottom &
 
-echo "Polybar launched..."
+polybar secondary_bottom &
+
