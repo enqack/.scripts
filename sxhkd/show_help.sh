28fd7da6e12195aff3da5094be914a6948ee673e
enqack <enqack@gmail.com>
2021-08-30 17:13:36 -0400

    Theme changes.

--- sxhkd/show_help.sh
+++ sxhkd/show_help.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat $HOME/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf
+cat $HOME/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf --color=16
 
