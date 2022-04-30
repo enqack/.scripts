efad3b7042bcdf5aa95ac38ae644553d1ce346f5
enqack <enqack@gmail.com>
2022-04-30 02:45:18 -0400

    Updated polybar launch script to work with split polybar config files. Added dynamic monitor assignment.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+HEAD_NAMES=$(polybar -M | cut -d ':' -f 1)
+
 killall -q polybar
 
+# wait for killall to take effect
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar primary &
+counter=1
+for head in $HEAD_NAMES; do
+  export MONITOR=$head
 
-polybar secondary &
+  if [ "$counter" -eq "1" ]; then
+    polybar primary --config=$HOME/.config/polybar/primary.ini &
+  fi
+
+  if [ "$counter" -eq "2" ]; then
+    polybar secondary --config=$HOME/.config/polybar/secondary.ini &
+  fi
+
+  if [ "$counter" -gt "2" ]; then
+    polybar additional --config=$HOME/.config/polybar/additional.ini &
+  fi
+
+  ((counter++))
+done
 
